ae62a0592daa58440160293bb3d83c3c98d6bc78
Cordero Dixon <lab@Corderos-MacBook-Pro.local>
2018-07-06 10:44:52 -0400

    express running

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('this should work');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('this works');
+});
+
+app.listen(3000, () => {
+  console.log('listening');
+});
